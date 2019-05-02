f71292a6952d88c477d65d9fc74e742193d90ae0
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 23:48:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |k, v|
-    if v < lowest_val
-      return k
+    #if v < lowest_val
+      #return k
     #elsif name_hash = []
     #  return nil
-end
+#end
 
   end
 
