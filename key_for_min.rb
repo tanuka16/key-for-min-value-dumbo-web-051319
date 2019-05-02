2a3de0347631dfa34600edc95e31e880d1a8f60f
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 23:47:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash.collect do |k, v|
+    if v < lowest_val
+      return k
+    #elsif name_hash = []
+    #  return nil
+end
 
+  end
+
+
+  #end
 end
