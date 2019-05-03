03f9801322989f1b4d8ab5aeeb1c8a5e23090f52
Tanuka Das <das.tanuka@yahoo.com>
2019-05-03 00:00:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
+  #get the first key:value pair by using .first method
   min_key, min_value = name_hash.first
   name_hash.collect do |k, v|
+    # iterate over each value to check which one smaller than the return value
     if v < min_value
-      min_key = k
+      min_key = k               #just need the key
     end
 
   end
